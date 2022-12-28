b274c95d1780f43566971c226df01db1a4d88645
Alexsander Pereira <alexsander.pereira@icloud.com>
2022-12-28 20:23:17 -0300

    Test CI/CD and creating README basic

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,5 @@ terraform {
   backend "gcs" {
     bucket      = "hurb-terraform"
     prefix      = "tfstate"
-    credentials = "credentials.json"
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,11 @@
 provider "google" {
   project     = "hurb-373018"
-  credentials = file("credentials.json")
   region      = "us-central1"
   zone        = "us-central1-a"
 }
 
 provider "google-beta" {
   project     = "hurb-373018"
-  credentials = file("credentials.json")
   region      = "us-central1"
   zone        = "us-central1-a"
 }
